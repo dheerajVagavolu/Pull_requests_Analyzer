6181dd300a1a30dd0cf82c6d5b9a17865e18bec3
V Dheeraj <41289602+dheerajrox@users.noreply.github.com>
2020-01-13 22:00:29 +0530

    Update parent.py

--- parent.py
+++ parent.py
@@ -8,7 +8,7 @@ cur = os.getcwd()
 
 for i in f:
     line = i.split(' ')
-    cmd1 = 'python3 sel.py '+line[0]+' '+line[1]
+    cmd1 = 'python sel.py '+line[0]+' '+line[1]
     os.system(cmd1)
     cmd2 = 'git clone '+line[2]
     os.system(cmd2)
@@ -21,7 +21,7 @@ for i in f:
     cmd5 = 'cp tags_dates_'+line[0].split('/')[1]+'.txt ../'
     os.system(cmd5)
     os.chdir(cur)
-    cmd5 = 'python3 dates_plot.py '+ line[0].split('/')[1] +'_logs_new.txt tags_dates_'+line[0].split('/')[1]+'.txt'
+    cmd5 = 'python dates_plot.py '+ line[0].split('/')[1] +'_logs_new.txt tags_dates_'+line[0].split('/')[1]+'.txt'
     print(cmd5)
     os.system(cmd5)
 
